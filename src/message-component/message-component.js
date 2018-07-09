025e4f30806b527130fca50edf2380c29d88386b
Adam Natale <adam.natale@mail.utoronto.ca>
2018-07-09 17:05:25 -0400

    Pushing beginning files

---
+++ src/message-component/message-component.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './message-component.css';
+
+export default class MessageComponent extends React.Component {
+    render() {
+        return (
+            <div id="msg">
+                <div id="obj">
+                    Rock
+                </div>
+            </div>
+        )
+    }
+}
