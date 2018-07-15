f2d060dcd3b814dbb0846a32617f3bae5ba5ce59
Adam Natale <adam.natale@mail.utoronto.ca>
2018-07-14 21:46:08 -0400

    Added icons for selections

--- src/message-component/message-component.js
+++ src/message-component/message-component.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import './message-component.css';
+import iconRock from './icons8-rock-48.png';
+import iconPaper from './icons8-sheet-of-paper-48.png';
+import iconScissors from './icons8-cutting-48.png';
 
 export default class MessageComponent extends React.Component {
     
     constructor(props) {
         super(props);
         this.weapon = props.data;
+        
+        switch(this.weapon) {
+            case 'Rock':
+                this.icon = iconRock;
+                break;
+            case 'Paper':
+                this.icon = iconPaper;
+                break;
+            default:
+                this.icon = iconScissors;
+                break;
+        }
     }
 
     test() {
@@ -19,6 +34,7 @@ export default class MessageComponent extends React.Component {
                 <div id="obj">
                     {this.props.data}
                 </div>
+                <img src={this.icon} alt="logo" />
                 
             </div>
         )
